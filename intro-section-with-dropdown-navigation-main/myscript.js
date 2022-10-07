f48b085590e29b30ace43594ba5391c5ea99160e
Lucas <lucasdiavolo2020@gmail.com>
2022-10-06 23:42:39 -0300

    add intro section with dropdown

---
+++ intro-section-with-dropdown-navigation-main/myscript.js
@@ -0,0 +1,12 @@
+function iconChange() {
+  document.getElementById("icon").src = "./images/icon-arrow-up.svg";
+}
+function iconChange2() {
+  document.getElementById("icon2").src = "./images/icon-arrow-up.svg";
+}
+function iconReturn() {
+  document.getElementById("icon").src = "./images/icon-arrow-down.svg";
+}
+function iconReturn2() {
+  document.getElementById("icon2").src = "./images/icon-arrow-down.svg";
+}
